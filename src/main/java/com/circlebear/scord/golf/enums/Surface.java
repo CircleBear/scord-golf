4d0fd17031967430f5301fd4d92198c070d22cda
Dan Wiechert <dan.wiechert@gmail.com>
2013-06-29 21:54:09 -0500

    Added surface enum.

---
+++ src/main/java/com/circlebear/scord/golf/enums/Surface.java
@@ -0,0 +1,23 @@
+package com.circlebear.scord.golf.enums;
+
+/**
+ * Enum to hold the different types of sufaces.
+ * 
+ * @author Dan Wiechert
+ */
+public enum Surface {
+	/** The fairway. */
+	FAIRWAY,
+	/** The green. */
+	GREEN,
+	/** The tee. */
+	TEE,
+	/** The rough. */
+	ROUGH,
+	/** The sand. */
+	SAND,
+	/** The water. */
+	WATER,
+	/** The woods. */
+	WOODS;
+}
